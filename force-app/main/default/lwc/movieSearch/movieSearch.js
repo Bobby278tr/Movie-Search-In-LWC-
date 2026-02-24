f6c3571eabe5ab86f3b85f7b463a73f0db012a94
Bobby Tripathi <bobbytripathi24@gmail.com>
2026-02-24 16:27:37 +0530

    Publishing movieId from move search comp to message channel

--- force-app/main/default/lwc/movieSearch/movieSearch.js
+++ force-app/main/default/lwc/movieSearch/movieSearch.js
@@ -1,4 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import {publish, MessageContext} from 'lightning/messageService';
+import MOVIE_CHANNEL from '@salesforce/messageChannel/movieChannel__c';
 
 const DELAY =300;
 export default class MovieSearch extends LightningElement {
@@ -9,6 +11,10 @@ export default class MovieSearch extends LightningElement {
     delayTimeout;
     serachResult = [];
     selectedMovie = "";
+
+    @wire(MessageContext)
+    messageContext;
+
     get typeOptions() {
         return [
             { label: 'None', value: '' },
@@ -53,5 +59,7 @@ export default class MovieSearch extends LightningElement {
 
     movieSelectedHandler(event){
         this.selectedMovie = event.detail;
+        const payload = {moveId: this.selectedMovie};
+        publish(this.messageContext, MOVIE_CHANNEL, payload);
     }
 }
