0a9306bb03a8c80f2b3f1ccfcc78871ac47a1756
Bobby Tripathi <bobbytripathi24@gmail.com>
2026-02-24 16:46:01 +0530

    Movie detail subscribing to the message channel to access the selected movieId

--- force-app/main/default/lwc/movieDetail/movieDetail.js
+++ force-app/main/default/lwc/movieDetail/movieDetail.js
@@ -1,5 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import {subscribe, MessageContext, unsubscribe, APPLICATION_SCOPE} from 'lightning/messageService';
+import MOVIE_CHANNEL from '@salesforce/messageChannel/movieChannel__c';
 
 export default class MovieDetail extends LightningElement {
+    @wire(MessageContext)
+    messageContext;
+    subscription = null;
+    movieId='';
+
+    connectedCallback(){
+        this.subscribeToMessageChannel();
+    }
+
+    disconnectedCallback(){
+        this.unSubscribeToMessageChannel();
+    }
+    
+    subscribeToMessageChannel(){
+        if(!this.subscription){
+            this.subscription = subscribe(
+                this.messageContext,
+                MOVIE_CHANNEL,
+                (message) => this.handleMessage(message),
+                {scope: APPLICATION_SCOPE}
+            );
+        }
+    }
+
+    handleMessage(message){
+        let movieId = message.moveId;
+        console.log('Movie Id', movieId);
+    }
+
+    unSubscribeToMessageChannel(){
+        if(this.subscription){
+            unsubscribe(this.subscription);
+            this.subscription = null;
+        }
+    }
     
 }
